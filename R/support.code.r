252ba80ae2b6ce9421ecadab3f2ab13c9e33276b
Michael Collyer <mlcollyer@gmail.com>
2016-09-13 07:17:40 -0400

    support code update

--- R/support.code.r
+++ R/support.code.r
@@ -19,6 +19,106 @@
 #' @section BIO532 TOC:
 #' BIO532-package
 NULL
+
+#' actions data from Pagano and Gauvreau 
+#'
+#' @name actions
+#' @docType data
+#' @references Principles of Biostatistics, 2nd ed., 
+#' by Marcello Pagano and Kimberlee Gauvreau (2000) 
+#' Pacific Grove, PA: Duxbury publishers. ISBN: 0-534-22902-6 
+#' U.S.A. 97:4106-4111
+#' @keywords datasets
+NULL
+
+#' africa data from Pagano and Gauvreau 
+#'
+#' @name africa
+#' @docType data
+#' @references Principles of Biostatistics, 2nd ed., 
+#' by Marcello Pagano and Kimberlee Gauvreau (2000) 
+#' Pacific Grove, PA: Duxbury publishers. ISBN: 0-534-22902-6 
+#' U.S.A. 97:4106-4111
+#' @keywords datasets
+NULL
+
+#' alcohol data from Pagano and Gauvreau 
+#'
+#' @name alcohol
+#' @docType data
+#' @references Principles of Biostatistics, 2nd ed., 
+#' by Marcello Pagano and Kimberlee Gauvreau (2000) 
+#' Pacific Grove, PA: Duxbury publishers. ISBN: 0-534-22902-6 
+#' U.S.A. 97:4106-4111
+#' @keywords datasets
+NULL
+
+#' angio data from Pagano and Gauvreau 
+#'
+#' @name angio
+#' @docType data
+#' @references Principles of Biostatistics, 2nd ed., 
+#' by Marcello Pagano and Kimberlee Gauvreau (2000) 
+#' Pacific Grove, PA: Duxbury publishers. ISBN: 0-534-22902-6 
+#' U.S.A. 97:4106-4111
+#' @keywords datasets
+NULL
+
+#' bed data from Pagano and Gauvreau 
+#'
+#' @name bed
+#' @docType data
+#' @references Principles of Biostatistics, 2nd ed., 
+#' by Marcello Pagano and Kimberlee Gauvreau (2000) 
+#' Pacific Grove, PA: Duxbury publishers. ISBN: 0-534-22902-6 
+#' U.S.A. 97:4106-4111
+#' @keywords datasets
+NULL
+
+#' bed2 data from Pagano and Gauvreau 
+#'
+#' @name bed2
+#' @docType data
+#' @references Principles of Biostatistics, 2nd ed., 
+#' by Marcello Pagano and Kimberlee Gauvreau (2000) 
+#' Pacific Grove, PA: Duxbury publishers. ISBN: 0-534-22902-6 
+#' U.S.A. 97:4106-4111
+#' @keywords datasets
+NULL
+
+#' bladder data from Pagano and Gauvreau 
+#'
+#' @name bladder
+#' @docType data
+#' @references Principles of Biostatistics, 2nd ed., 
+#' by Marcello Pagano and Kimberlee Gauvreau (2000) 
+#' Pacific Grove, PA: Duxbury publishers. ISBN: 0-534-22902-6 
+#' U.S.A. 97:4106-4111
+#' @keywords datasets
+NULL
+
+#' brate data from Pagano and Gauvreau 
+#'
+#' @name brate
+#' @docType data
+#' @references Principles of Biostatistics, 2nd ed., 
+#' by Marcello Pagano and Kimberlee Gauvreau (2000) 
+#' Pacific Grove, PA: Duxbury publishers. ISBN: 0-534-22902-6 
+#' U.S.A. 97:4106-4111
+#' @keywords datasets
+NULL
+
+#' cad data from Pagano and Gauvreau 
+#'
+#' @name cad
+#' @docType data
+#' @references Principles of Biostatistics, 2nd ed., 
+#' by Marcello Pagano and Kimberlee Gauvreau (2000) 
+#' Pacific Grove, PA: Duxbury publishers. ISBN: 0-534-22902-6 
+#' U.S.A. 97:4106-4111
+#' @keywords datasets
+NULL
+
 #' cigarett data from Pagano and Gauvreau 
 #'
 #' @name cigarett
@@ -29,3 +129,247 @@ NULL
 #' U.S.A. 97:4106-4111
 #' @keywords datasets
 NULL
+
+#' cyto data from Pagano and Gauvreau 
+#'
+#' @name cyto
+#' @docType data
+#' @references Principles of Biostatistics, 2nd ed., 
+#' by Marcello Pagano and Kimberlee Gauvreau (2000) 
+#' Pacific Grove, PA: Duxbury publishers. ISBN: 0-534-22902-6 
+#' U.S.A. 97:4106-4111
+#' @keywords datasets
+NULL
+
+#' detroit data from Pagano and Gauvreau 
+#'
+#' @name detroit
+#' @docType data
+#' @references Principles of Biostatistics, 2nd ed., 
+#' by Marcello Pagano and Kimberlee Gauvreau (2000) 
+#' Pacific Grove, PA: Duxbury publishers. ISBN: 0-534-22902-6 
+#' U.S.A. 97:4106-4111
+#' @keywords datasets
+NULL
+
+#' diabetes data from Pagano and Gauvreau 
+#'
+#' @name diabetes
+#' @docType data
+#' @references Principles of Biostatistics, 2nd ed., 
+#' by Marcello Pagano and Kimberlee Gauvreau (2000) 
+#' Pacific Grove, PA: Duxbury publishers. ISBN: 0-534-22902-6 
+#' U.S.A. 97:4106-4111
+#' @keywords datasets
+NULL
+
+#' dialysis data from Pagano and Gauvreau 
+#'
+#' @name dialysis
+#' @docType data
+#' @references Principles of Biostatistics, 2nd ed., 
+#' by Marcello Pagano and Kimberlee Gauvreau (2000) 
+#' Pacific Grove, PA: Duxbury publishers. ISBN: 0-534-22902-6 
+#' U.S.A. 97:4106-4111
+#' @keywords datasets
+NULL
+
+#' dthrate data from Pagano and Gauvreau 
+#'
+#' @name dthrate
+#' @docType data
+#' @references Principles of Biostatistics, 2nd ed., 
+#' by Marcello Pagano and Kimberlee Gauvreau (2000) 
+#' Pacific Grove, PA: Duxbury publishers. ISBN: 0-534-22902-6 
+#' U.S.A. 97:4106-4111
+#' @keywords datasets
+NULL
+
+#' heart data from Pagano and Gauvreau 
+#'
+#' @name heart
+#' @docType data
+#' @references Principles of Biostatistics, 2nd ed., 
+#' by Marcello Pagano and Kimberlee Gauvreau (2000) 
+#' Pacific Grove, PA: Duxbury publishers. ISBN: 0-534-22902-6 
+#' U.S.A. 97:4106-4111
+#' @keywords datasets
+NULL
+
+#' hospital data from Pagano and Gauvreau 
+#'
+#' @name hospital
+#' @docType data
+#' @references Principles of Biostatistics, 2nd ed., 
+#' by Marcello Pagano and Kimberlee Gauvreau (2000) 
+#' Pacific Grove, PA: Duxbury publishers. ISBN: 0-534-22902-6 
+#' U.S.A. 97:4106-4111
+#' @keywords datasets
+NULL
+
+#' insure data from Pagano and Gauvreau 
+#'
+#' @name insure
+#' @docType data
+#' @references Principles of Biostatistics, 2nd ed., 
+#' by Marcello Pagano and Kimberlee Gauvreau (2000) 
+#' Pacific Grove, PA: Duxbury publishers. ISBN: 0-534-22902-6 
+#' U.S.A. 97:4106-4111
+#' @keywords datasets
+NULL
+
+#' ischemic data from Pagano and Gauvreau 
+#'
+#' @name ischemic
+#' @docType data
+#' @references Principles of Biostatistics, 2nd ed., 
+#' by Marcello Pagano and Kimberlee Gauvreau (2000) 
+#' Pacific Grove, PA: Duxbury publishers. ISBN: 0-534-22902-6 
+#' U.S.A. 97:4106-4111
+#' @keywords datasets
+NULL
+
+#' lifeexp data from Pagano and Gauvreau 
+#'
+#' @name lifeexp
+#' @docType data
+#' @references Principles of Biostatistics, 2nd ed., 
+#' by Marcello Pagano and Kimberlee Gauvreau (2000) 
+#' Pacific Grove, PA: Duxbury publishers. ISBN: 0-534-22902-6 
+#' U.S.A. 97:4106-4111
+#' @keywords datasets
+NULL
+
+#' liferace data from Pagano and Gauvreau 
+#'
+#' @name liferace
+#' @docType data
+#' @references Principles of Biostatistics, 2nd ed., 
+#' by Marcello Pagano and Kimberlee Gauvreau (2000) 
+#' Pacific Grove, PA: Duxbury publishers. ISBN: 0-534-22902-6 
+#' U.S.A. 97:4106-4111
+#' @keywords datasets
+NULL
+
+#' lowbwt data from Pagano and Gauvreau 
+#'
+#' @name lowbwt
+#' @docType data
+#' @references Principles of Biostatistics, 2nd ed., 
+#' by Marcello Pagano and Kimberlee Gauvreau (2000) 
+#' Pacific Grove, PA: Duxbury publishers. ISBN: 0-534-22902-6 
+#' U.S.A. 97:4106-4111
+#' @keywords datasets
+NULL
+
+
+#' miner data from Pagano and Gauvreau 
+#'
+#' @name miner
+#' @docType data
+#' @references Principles of Biostatistics, 2nd ed., 
+#' by Marcello Pagano and Kimberlee Gauvreau (2000) 
+#' Pacific Grove, PA: Duxbury publishers. ISBN: 0-534-22902-6 
+#' U.S.A. 97:4106-4111
+#' @keywords datasets
+NULL
+
+#' nurshome data from Pagano and Gauvreau 
+#'
+#' @name nurshome
+#' @docType data
+#' @references Principles of Biostatistics, 2nd ed., 
+#' by Marcello Pagano and Kimberlee Gauvreau (2000) 
+#' Pacific Grove, PA: Duxbury publishers. ISBN: 0-534-22902-6 
+#' U.S.A. 97:4106-4111
+#' @keywords datasets
+NULL
+
+#' prison data from Pagano and Gauvreau 
+#'
+#' @name prison
+#' @docType data
+#' @references Principles of Biostatistics, 2nd ed., 
+#' by Marcello Pagano and Kimberlee Gauvreau (2000) 
+#' Pacific Grove, PA: Duxbury publishers. ISBN: 0-534-22902-6 
+#' U.S.A. 97:4106-4111
+#' @keywords datasets
+NULL
+
+#' program data from Pagano and Gauvreau 
+#'
+#' @name prgram
+#' @docType data
+#' @references Principles of Biostatistics, 2nd ed., 
+#' by Marcello Pagano and Kimberlee Gauvreau (2000) 
+#' Pacific Grove, PA: Duxbury publishers. ISBN: 0-534-22902-6 
+#' U.S.A. 97:4106-4111
+#' @keywords datasets
+NULL
+
+#' serzinc data from Pagano and Gauvreau 
+#'
+#' @name serzinc
+#' @docType data
+#' @references Principles of Biostatistics, 2nd ed., 
+#' by Marcello Pagano and Kimberlee Gauvreau (2000) 
+#' Pacific Grove, PA: Duxbury publishers. ISBN: 0-534-22902-6 
+#' U.S.A. 97:4106-4111
+#' @keywords datasets
+NULL
+
+#' stenosis data from Pagano and Gauvreau 
+#'
+#' @name stenosis
+#' @docType data
+#' @references Principles of Biostatistics, 2nd ed., 
+#' by Marcello Pagano and Kimberlee Gauvreau (2000) 
+#' Pacific Grove, PA: Duxbury publishers. ISBN: 0-534-22902-6 
+#' U.S.A. 97:4106-4111
+#' @keywords datasets
+NULL
+
+#' twenty data from Pagano and Gauvreau 
+#'
+#' @name twenty
+#' @docType data
+#' @references Principles of Biostatistics, 2nd ed., 
+#' by Marcello Pagano and Kimberlee Gauvreau (2000) 
+#' Pacific Grove, PA: Duxbury publishers. ISBN: 0-534-22902-6 
+#' U.S.A. 97:4106-4111
+#' @keywords datasets
+NULL
+
+#' unicef data from Pagano and Gauvreau 
+#'
+#' @name unicef
+#' @docType data
+#' @references Principles of Biostatistics, 2nd ed., 
+#' by Marcello Pagano and Kimberlee Gauvreau (2000) 
+#' Pacific Grove, PA: Duxbury publishers. ISBN: 0-534-22902-6 
+#' U.S.A. 97:4106-4111
+#' @keywords datasets
+NULL
+
+#' us1940 data from Pagano and Gauvreau 
+#'
+#' @name us1940
+#' @docType data
+#' @references Principles of Biostatistics, 2nd ed., 
+#' by Marcello Pagano and Kimberlee Gauvreau (2000) 
+#' Pacific Grove, PA: Duxbury publishers. ISBN: 0-534-22902-6 
+#' U.S.A. 97:4106-4111
+#' @keywords datasets
+NULL
+
+#' water data from Pagano and Gauvreau 
+#'
+#' @name water
+#' @docType data
+#' @references Principles of Biostatistics, 2nd ed., 
+#' by Marcello Pagano and Kimberlee Gauvreau (2000) 
+#' Pacific Grove, PA: Duxbury publishers. ISBN: 0-534-22902-6 
+#' U.S.A. 97:4106-4111
+#' @keywords datasets
+NULL
+
