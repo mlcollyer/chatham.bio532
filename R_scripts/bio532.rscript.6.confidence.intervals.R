b3786e98e2ed7ec17adf1c58c2f8604b5e73a330
Michael Collyer <mlcollyer@gmail.com>
2016-09-20 11:08:34 -0400

    changed plot.confidence.interval

--- R_scripts/bio532.rscript.6.confidence.intervals.R
+++ R_scripts/bio532.rscript.6.confidence.intervals.R
@@ -56,80 +56,11 @@ sd(Y)/sqrt(n)
 
 # Using our sample, y
 
-sample(y, replace = TRUE)
+library(chatham.bio532)
 
-# The replace = TRUE part is important.  If FALSE, the process just shuffles the data; 
-# if TRUE, it creates a new hypothetical sample from the data
+yCI <- confidence.int(y)
 
-# Let's do this many times (don't worry about code too much here)
+yCI$random.samples
+yCI$means
 
-perms = 1000
-resample = function(.) sample(y, size=n, replace = TRUE)
-y.r = lapply(1:perms, resample)
-
-y.r # 1000 resamples of y
-
-
-
-# Let's look at the interquartile range of the sampling distribution of means
-
-IQR(ySamples$means)
-summary(ySamples$means)
-
-# How do we intrpret this information?  We know that 50% of the values fall within the IQR,
-# but does that tell us anything?
-
-# YES!
-
-# Because the sampling distribution is also a probability distribution, it gives us a probability
-# of capturing the true population mean from our sampling.  We should be ~50% confident
-# that the true population mean falls within this range.
-
-mean(Y) # population mean
-
-
-ySamples$samples
-
-# Can plot too
-plot(ySamples)
-
-# A better example... yes, 10,000 permutations!
-
-ySamples = multisample(population = Y, size = n, permutations = 10000, CLT=TRUE)
-summary(ySamples)
-plot(ySamples, breaks = 50, col="dark green")
-
-# Let's do this again with a different kind of distribution
-
-Y = rpois(10000, lambda =2) # Poisson distribution
-hist(Y, col="orange") # not normal!
-
-ySamples = multisample(population = Y, size = n, permutations = 10000, CLT=TRUE)
-summary(ySamples)
-plot(ySamples, breaks = 50, col="dark orange")
-
-# Let's go crazy!
-ySamples = multisample(population = Y, size = n, permutations = 50000, CLT=TRUE)
-summary(ySamples)
-plot(ySamples, breaks = 50, col="dark orange")
-
-# Remember, a Poisson distribution is discrete, not continuous
-# Nevertheless, the CLT still holds, pretty much
-
-# Another
-
-Y = rlnorm(10000, mean = 2, sd = 0.5) # Lognormal distribution
-hist(Y, col="yellow") # not normal!
-
-ySamples = multisample(population = Y, size = n, permutations = 10000, CLT=TRUE)
-summary(ySamples)
-plot(ySamples, breaks = 50, col=" yellow")
-
-#-----------------------------------------------------------------------------------
-# Parting thoughts
-
-# 1) CLT important!  Repat, CLT important!
-# 2) Theory tells us what to expect if we were to sample a population many times
-# 3) Therefore, we can assume som ethings about statistics we estimate for population 
-# apramters.  This will be important when we wish to comapre population parameters
-# in inferrential tests.
+plot(yCI)
